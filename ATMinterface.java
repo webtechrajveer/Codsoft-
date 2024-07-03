a581855482a95f9293b09531b68f8d7f7502773f
Rajveer <rajvirjaat1104@gmail.com>
2024-07-03 18:39:35 +0530

    Add files via upload
    Task Third ATM Interface Upload

---
+++ ATMinterface.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class ATMinterface {
+    private static ATM atm;
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1000); // Initial balance
+        atm = new ATM(account);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("ATM Interface");
+            System.out.println("1. Withdraw");
+            System.out.println("2. Deposit");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter amount to withdraw: ");
+                    double withdrawAmount = scanner.nextDouble();
+                    atm.withdraw(withdrawAmount);
+                    break;
+                case 2:
+                    System.out.print("Enter amount to deposit: ");
+                    double depositAmount = scanner.nextDouble();
+                    atm.deposit(depositAmount);
+                    break;
+                case 3:
+                    atm.checkBalance();
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+    }
+}
+
+class ATM {
+    private BankAccount account;
+
+    public ATM(BankAccount account) {
+        this.account = account;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > account.getBalance()) {
+            System.out.println("Insufficient balance.");
+        } else {
+            account.withdraw(amount);
+            System.out.println("Withdrawal successful. Current balance: " + account.getBalance());
+        }
+    }
+
+    public void deposit(double amount) {
+        account.deposit(amount);
+        System.out.println("Deposit successful. Current balance: " + account.getBalance());
+    }
+
+    public void checkBalance() {
+        System.out.println("Current balance: " + account.getBalance());
+    }
+}
+
+class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+}
